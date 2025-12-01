56a4d9f1f3afae9788b1a6277b7a69b74f6e8b30
Marius Avignant <marius.avignant@gmail.com>
2025-12-01 13:40:14 +0100

    Ajout d_un bouton sources

--- imc-app/src/pages/Home/InfoImc.tsx
+++ imc-app/src/pages/Home/InfoImc.tsx
@@ -2,7 +2,16 @@
 const InfoImc = () => {
   return (
     <div>
-      <h1>Informations: </h1>
+      <div>
+        <h2>Comment l'IMC est calculé : </h2>
+        <p>
+          L'Indice de Masse Corporelle (IMC) est calculé en divisant le poids
+          d'une personne en kilogrammes par le carré de sa taille en mètres.
+        </p>
+      </div>
+      <div>
+        <a href="https://fr.wikipedia.org/wiki/Indice_de_masse_corporelle" target="_blank" rel="noopener noreferrer">Qu'est ce que l'IMC ?</a>
+      </div>
     </div>
   );
 };
