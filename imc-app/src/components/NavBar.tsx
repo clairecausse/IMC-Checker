8b0faf20a9582e8f4d940a6c62a5300203974cfe
Marius Avignant <marius.avignant@gmail.com>
2025-12-01 14:26:47 +0100

    US-05

--- imc-app/src/components/NavBar.tsx
+++ imc-app/src/components/NavBar.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
     <nav>
+      <img src={logo} alt="Logo" />
       <ul>
         <li><Link to="/">Accueil</Link></li>
         <li><Link to="/historique">Historique</Link></li>
--- imc-app/src/pages/Home/Home.tsx
+++ imc-app/src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import Calculator from "./Calculator";
 import InfoImc from "./InfoImc";
 import Partners from "./Partners";
+import logo from "../../assets/logo.png";
 
 const Home = () => {
   return (
     <div>
+      <img src={logo} alt="Logo" style={{ maxWidth: "200px", margin: "2rem auto", display: "block" }} />
       <h1>Accueil</h1>
       <Calculator />
       <InfoImc />
