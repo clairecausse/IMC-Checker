10db5b65484c7687f1a9c4db06f2623b5bd5f3e5
Claire CAUSSE <claire.causse@alumni.univ-avignon.fr>
2025-12-08 17:13:05 +0100

    Fix databse on windows

--- imc-app/backend/database/index.js
+++ imc-app/backend/database/index.js
@@ -1,7 +1,9 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 
-export default await open({
+const db = await open({
   filename: "database.db",
   driver: sqlite3.Database
 });
+
+export default db;
